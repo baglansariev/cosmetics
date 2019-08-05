6eb57eed444535d9841ed8c586e9e34753e5edc0
baglansariev <baglansariev@mail.ru>
2019-08-05 19:54:55 +0600

    project start

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -1,3 +1,27 @@
 <header id="header">
-	<h1>Stinger Framework</h1>
+    <div class="header-top">
+        <div class="top-social-networks">
+            <a href="">
+                <i class="fab fa-instagram"></i>
+            </a>
+            <a href="">
+                <i class="fab fa-vk"></i>
+            </a>
+            <a href="mailto:baglansariev@mail.ru">
+                <i class="far fa-envelope"></i>
+            </a>
+        </div>
+        <div class="main-logo">
+            <a href="/">
+                <img src="/public/images/logo.png" alt="">
+            </a>
+        </div>
+        <div class="top-phone">
+            <a href="tel:+77024441143">
+                <i class="fas fa-phone-alt"></i>
+                213-334-3688
+            </a>
+        </div>
+    </div>
+    <div class="header-bottom"></div>
 </header>
--- application/views/layouts/default.php
+++ application/views/layouts/default.php
@@ -8,6 +8,8 @@
 	<meta name="keywords" content="<?= $this->asset->getMetaKeys() ?>">
 	<title><?= $this->asset->getTitle() ?></title>
 	<link rel="shortcut icon" href="/public/images/icons/favicon.png" type="image/x-icon">
+    <link href="https://fonts.googleapis.com/css?family=Didact+Gothic&display=swap" rel="stylesheet">
+    <link  rel="stylesheet" href="/public/style/font-awesome/css/all.min.css">
 	<link rel="stylesheet" href="/public/style/css/main.css">
 	<link rel="stylesheet" href="/public/style/bootstrap/bootstrap-grid.css">
 	<link rel="stylesheet" href="/public/style/bootstrap/bootstrap.css">
@@ -15,6 +17,7 @@
 	<link rel="stylesheet" href="<?= $css ?>">
 	<? endforeach ?>
 	<script type="text/javascript" src="/public/style/js/jquery-3.4.0.min.js"></script>
+    <script type="text/javascript" src="/public/style/font-awesome/js/all.min.js"></script>
 	<script type="text/javascript" src="/public/style/js/app.js"></script>
 	<? foreach($this->asset->getJs() as $js): ?>
 	<script type="text/javascript" src="<?= $js ?>"></script>
