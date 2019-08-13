a579ab917e5c0ebbc52f9e9b16e1ae3f6462c6fe
baglansariev <baglansariev@mail.ru>
2019-08-13 16:41:54 +0600

    Yandex search tag has been added

--- application/views/layouts/default.php
+++ application/views/layouts/default.php
@@ -4,6 +4,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="yandex-verification" content="6a4af72c2e26a571" />
 	<meta name="description" content="<?= $this->asset->getMetaDesc() ?>">
 	<meta name="keywords" content="<?= $this->asset->getMetaKeys() ?>">
 	<title><?= $this->asset->getTitle() ?></title>
